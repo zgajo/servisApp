bb65b571913034c0680b976d48cacd0147d53776
Korisnik <Korisnik@Servisnjic>
2016-11-09 10:28:18 +0100

    test

--- pageParts/primkaPagePart/uredi_primku.php
+++ pageParts/primkaPagePart/uredi_primku.php
@@ -6,6 +6,7 @@ if(empty($primka[0]['primka_id'])){ unset($primka) ; $primka=new primka(); $prim
 
 $datumZaprimanja = date("d.m.Y / H:i:s", strtotime($primka[0]['datumZaprimanja']));
 
+
 if($primka[0]['datumKupnje'] === "0000-00-00"){
     $kupljeno=NULL;
 }else{
